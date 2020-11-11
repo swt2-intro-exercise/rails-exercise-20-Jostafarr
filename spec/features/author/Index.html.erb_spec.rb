f27b9339b81a26e399a2441f17dac9c97e662e5e
jostafarr <jost.goette@student.hpi.de>
2020-11-11 20:28:38 +0100

    added test that tests if the author index page renders

--- spec/features/author/Index.html.erb_spec.rb
+++ spec/features/author/Index.html.erb_spec.rb
@@ -2,7 +2,9 @@
 require 'rails_helper'
 
 describe 'author index page', type: :feature do
-
+  it 'author index page should render' do
+    visit authors_path
+  end
   it 'have link to add new authors' do
     visit authors_path
     expect(page).to have_link 'New', href: new_author_path
