65ca350cf10c6924dde65925a32cacfa51f3d9ff
jostafarr <jost.goette@student.hpi.de>
2020-11-26 16:25:28 +0100

    tried to fixed the test

--- spec/controllers/papers_controller_spec.rb
+++ spec/controllers/papers_controller_spec.rb
@@ -59,7 +59,7 @@ RSpec.describe PapersController, type: :controller do
 
   describe "GET #new" do
     it "returns a success response" do
-      get :new, params: {}, session: valid_session
+      get :new, params: {paper: valid_attributes}, session: valid_session
       expect(response).to be_successful
     end
   end
